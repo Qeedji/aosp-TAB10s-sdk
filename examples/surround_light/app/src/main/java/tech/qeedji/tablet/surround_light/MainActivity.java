735889c060931b6c9819b4e5e1c896bb4a99a8be
Yoann Guerin <yoann.guerin@innes.fr>
2020-09-04 16:36:02 +0200

    add surround_light

---
+++ examples/surround_light/app/src/main/java/tech/qeedji/tablet/surround_light/MainActivity.java
@@ -0,0 +1,68 @@
+package tech.qeedji.tablet.surround_light;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+
+import tech.qeedji.system.lib.SurroundLight;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Light";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        SurroundLight surroundlight = new SurroundLight(this);
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioButton1:
+                if (checked) {
+                    Log.i(TAG, "Set red");
+                    surroundlight.setColor(SurroundLight.RED);
+                }
+                break;
+            case R.id.radioButton2:
+                if (checked) {
+                    Log.i(TAG, "Set green");
+                    surroundlight.setColor(SurroundLight.GREEN);
+                }
+                break;
+            case R.id.radioButton3:
+                if (checked) {
+                    Log.i(TAG, "Set orange");
+                    surroundlight.setColor(SurroundLight.ORANGE);
+                }
+                break;
+            case R.id.radioButton4:
+                if (checked) {
+                    Log.i(TAG, "Set off");
+                    surroundlight.setColor(SurroundLight.OFF);
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "resume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "pause");
+    }
+}
